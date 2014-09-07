b21475294c9668c3170373194e791738198a4e24
josephb57 <josephb57@gmail.com>
2014-09-06 20:05:18 -0600

    Initial commit

---
+++ Test.html_files/map.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="This is a test" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1410053190751">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" POSITION="right">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="hide_edge" COLOR="#808080" WIDTH="thin"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" POSITION="right">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<icon BUILTIN="yes"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" POSITION="right">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="Test 1" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" POSITION="right" ID="ID_361163047" CREATED="1410053216288" MODIFIED="1410053219358">
+<edge STYLE="bezier" COLOR="#ff0000" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<node TEXT="Test 1-1" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" ID="ID_541037001" CREATED="1410053226040" MODIFIED="1410053228589">
+<edge STYLE="bezier" COLOR="#ff0000" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</node>
+</node>
+<node TEXT="Test 2" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" POSITION="right" ID="ID_1725903953" CREATED="1410053219913" MODIFIED="1410053221997">
+<edge STYLE="bezier" COLOR="#0000ff" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<node TEXT="Test 2-1" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600" MIN_WIDTH="1" ID="ID_1448984867" CREATED="1410053230295" MODIFIED="1410053234765">
+<edge STYLE="bezier" COLOR="#0000ff" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</node>
+</node>
+</node>
+</map>
